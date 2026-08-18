9a068cabbd6feadfee154de10f530d0a9be06a6e
Fran Perocheau <francois.perocheau@...>
2026-08-18 16:55:59 +0200

    feat: refonte distinguer-cod-coi en 3 niveaux (homophones-niveaux)
    Migre la carte placeholder "distinguer-cod-coi" vers le moteur
    homophones-niveaux (CM1/CM2/6e), simplifie les libellés de consigne
    groupe-sujet et retire deux compétences de la carte sujet.

--- data/index.js
+++ data/index.js
@@ -22,6 +22,6 @@
     "data/vocabulaire.js"
   ];
   DOMAIN_FILES.forEach(function (src) {
-    document.write('<script src="' + src + '?v=7"></script>');
+    document.write('<script src="' + src + '?v=8"></script>');
   });
 })();
